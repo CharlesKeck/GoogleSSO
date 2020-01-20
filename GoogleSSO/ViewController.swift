675d6214ee2fa76c471e7e5c5d9d73518bce0948
Charles <charjami@hotmail.com>
2020-01-20 14:10:29 +0800

    Initial Commit

---
+++ GoogleSSO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSSO
+//
+//  Created by Charles on 20/1/20.
+//  Copyright © 2020 Charles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
